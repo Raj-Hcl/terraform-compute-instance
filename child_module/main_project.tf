d68126c50393fe6a08dcc26768ce4cb403635ea4
Raj-Hcl <raj.tripathi@hcltech.com>
2024-05-13 15:12:31 +0530

    Update main_project.tf

--- child_module/main_project.tf
+++ child_module/main_project.tf
@@ -1,3 +1,24 @@
 module "vm_module"{
     source = "../parent_module"
 }
+
+output "instancename" {
+  value = module.vm_module.instance_name
+}
+
+output "machinetype" {
+  value = module.vm_module.machine_type
+}
+
+output "instancezone" {
+  value = module.vm_module.zone
+}
+
+output "instance_bootdisk" {
+  value = module.vm_module.boot_disk
+  sensitive = true
+}
+
+output "instancenetwork" {
+  value = module.vm_module.network
+}
