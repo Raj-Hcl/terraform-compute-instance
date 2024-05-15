dc215d56fe8915bc1d919cc17f01b824f1942c40
Raj-Hcl <raj.tripathi@hcltech.com>
2024-05-15 15:15:33 +0530

    Update main_project.tf

--- child_module/main_project.tf
+++ child_module/main_project.tf
@@ -1,5 +1,22 @@
 module "vm_module"{
     source = "../parent_module"
+
+    myinstance = {
+        instance1 = {
+            name = "myinstance1"
+            machine_type = "e2-medium"
+            zone = "us-east1-a"
+            boot_disk_image = "debian-cloud/debian-10"
+            network = "default"
+        }
+        instance2 = {
+           name = "myinstance2"
+            machine_type = "e2-medium"
+            zone = "us-east1-b"
+            boot_disk_image = "debian-cloud/debian-10"
+            network = "default" 
+        }
+    }
 }
 
 output "instancename" {
