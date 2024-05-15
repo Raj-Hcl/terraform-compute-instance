60f004a3aa4bc709b2bfbd7ddbdb7ba22b92f91e
Raj-Hcl <raj.tripathi@hcltech.com>
2024-05-15 15:21:56 +0530

    Update variables.tf

--- parent_module/variables.tf
+++ parent_module/variables.tf
@@ -1,25 +1,25 @@
 variable "myinstance" {
     type = map(object({
-        name         = string
-        machine_type = string
-        zone         = string
+        name            = string
+        machine_type    = string
+        zone            = string
         boot_disk_image = string
         network         = string
     }))
     default = {
         "instance1" = {
-            name           = "instance1"
-            machine_type   = "n1-standard-1"
-            zone           = "us-central1-a"
+            name            = "instance1"
+            machine_type    = "n1-standard-1"
+            zone            = "us-central1-a"
             boot_disk_image = "debian-cloud/debian-10"
-            network        = "default"
+            network         = "default"
         }
         "instance2" = {
-            name           = "instance2"
-            machine_type   = "e2-medium"
-            zone           = "us-west1-a"
+            name            = "instance2"
+            machine_type    = "e2-medium"
+            zone            = "us-west1-a"
             boot_disk_image = "debian-cloud/debian-10"
-            network        = "default"
+            network         = "default"
         }
     }
 }
