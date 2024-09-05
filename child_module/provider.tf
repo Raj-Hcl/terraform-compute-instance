ba0804f971e19e29b2d74f970659b58e19654c59
Raj-Hcl <raj.tripathi@hcltech.com>
2024-09-05 15:54:14 +0530

    Delete child_module directory

--- child_module/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "google" {
-    credentials = "keys2.json"
-    project     = "qwiklabs-gcp-03-58cd1cd10301"
-}
--- child_module/providerr.tf
+++
@@ -1,4 +0,0 @@
-provider "google" {
-    #credentials = "keys2.json"
-    project     = "refreshing-oven-432114-i0"
-}
